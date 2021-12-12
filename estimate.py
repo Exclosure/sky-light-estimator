948d4a428af599ca51c2a54e5ea09b9defb0edd2
meawoppl <meawoppl@gmail.com>
2021-12-12 11:38:15 -0800

    Left over crufty

--- estimate.py
+++ estimate.py
@@ -21,12 +21,6 @@ def interp_lat_lon(lat: float, lon: float) -> tuple:
     return light_data[0,  lat_idx, lon_idx].to_numpy()
 
 
-def uglymax(thing):
-    max_v = 0
-    for a in thing:
-        pass
-
-
 print(light_data.rio.bounds())
 
 locations = {
@@ -42,9 +36,9 @@ if __name__ == "__main__":
         print(name, interp_lat_lon(loc[0], loc[1]))
 
     # Brightest spot
-    a = light_data.as_numpy()
-    indices = np.unravel_index(a.argmax(), a.shape)
-    print(light_data[indices])
+    # a = light_data.as_numpy()
+    # indices = np.unravel_index(a.argmax(), a.shape)
+    # print(light_data[indices])
 
     lat = float(input("Latitude:"))
     lon = float(input("Longitude:"))
