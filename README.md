56cd4af0b2300267c8b61c32a0efc73caf6a469e
meawoppl <meawoppl@gmail.com>
2021-12-12 10:57:47 -0800

    Estimation tool

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sky-light-estimator
 A quick tool for estimating the amount of light pollution given a particular lat/lon
+Data interpolated is available from this URL: https://eogdata.mines.edu/nighttime_light/annual/v20/
+
+
---
+++ estimate.py
@@ -0,0 +1,37 @@
+import os
+import rioxarray as rxr
+import earthpy as et
+
+light_data = rxr.open_rasterio("VNL_v2_npp_2020_global_vcmslcfg_c202102150000.median_masked.tif",
+                               masked=True)
+
+
+def linear_interp(x: float, x0: float, x1: float, y0: float, y1: float) -> float:
+    m = (y1 - y0) / (x1 - x0)
+    return y0 + (x - x0) * m
+
+
+def interp_lat_lon(lat: float, lon: float) -> tuple:
+    c, lat_size, lon_size = light_data.shape
+    lat_idx = int(linear_interp(lat, 75, -65, 0, lat_size))
+    lon_idx = int(linear_interp(lon, -180, 180, 0, lon_size))
+
+    return light_data[0,  lat_idx, lon_idx].to_numpy()
+
+
+print(light_data.rio.bounds())
+
+locations = {
+    "NYC": (40.730610, -73.935242),
+    "Vegas": (36.114647, -115.172813),
+    "null island": (0, 0),
+    "Riyadh": (24.774265, 46.738586)
+}
+
+if __name__ == "__main__":
+    for name, loc in locations.items():
+        print(name, interp_lat_lon(loc[0], loc[1]))
+
+    lat = float(input("Latitude:"))
+    lon = float(input("Longitude:"))
+    print("Score", interp_lat_lon(lat, lon))
